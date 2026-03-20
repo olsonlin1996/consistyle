4f0221a94c172255bf47538419b6f0a1ac0173b8
olsonlin1996 <olsonlin1996@gmail.com>
2026-03-20 10:27:37 +0800

    feat: synchronize FFT logic to inject_anchors and clean up code for better clarity

--- attn_utils.py
+++ attn_utils.py
@@ -61,7 +61,7 @@ class FeatureInjector:
         self.attn_masks = attn_masks
 
         self.inject_range_alpha = inject_range_alpha if isinstance(inject_range_alpha, list) else [inject_range_alpha]
-        self.swap_strategy = swap_strategy # 'min / 'mean' / 'first'
+        self.swap_strategy = swap_strategy 
         self.dist_thr = dist_thr
         self.inject_unet_parts = inject_unet_parts
         self.inject_res = [64]
@@ -81,7 +81,6 @@ class FeatureInjector:
         
         curr_mapping = extended_mapping[i]
 
-        # If the current image is not mapped to any other image, skip
         if not torch.any(torch.cat([curr_mapping[:i], curr_mapping[i+1:]])):
             return None
 
@@ -99,7 +98,6 @@ class FeatureInjector:
     def inject_outputs(self, output, curr_iter, output_res, extended_mapping, place_in_unet, anchors_cache=None):
         curr_unet_part = place_in_unet.split('_')[0]
 
-        # Inject only in the specified unet parts (up, mid, down)
         if (curr_unet_part not in self.inject_unet_parts) or output_res not in self.inject_res:
             return output
 
@@ -111,63 +109,36 @@ class FeatureInjector:
 
         alpha = next((alpha for min_range, max_range, alpha in self.inject_range_alpha if min_range <= curr_iter <= max_range), None)
         if alpha:
-            old_output = output#.clone()
+            old_output = output
             for i in range(bsz):
                 if self.swap_strategy == 'min':
                     curr_mapping = extended_mapping[i]
-
-                    # If the current image is not mapped to any other image, skip
                     if not torch.any(torch.cat([curr_mapping[:i], curr_mapping[i+1:]])):
                         continue
 
                     min_dists = nn_distances[i][curr_mapping].argmin(dim=0)
                     curr_nn_map = nn_map[i][curr_mapping][min_dists, torch.arange(vector_dim)]
-
                     curr_nn_distances = nn_distances[i][curr_mapping][min_dists, torch.arange(vector_dim)]
                     dist_thr = get_dynamic_threshold(curr_nn_distances) if self.dist_thr == 'dynamic' else self.dist_thr
                     dist_mask = curr_nn_distances < dist_thr
                     final_mask_tgt = attn_masks[i] & dist_mask
 
                     if self.use_freq_decouple and output_res == 64:
-                        # FFT Decomposition Path (Only apply to the target mask area)
                         dim = output.shape[-1]
-                        
-                        # Reshape to spatial
                         tgt_spatial = output[i].reshape(64, 64, dim)
                         ref_all = old_output[curr_mapping][min_dists, curr_nn_map]
                         ref_spatial = ref_all.reshape(64, 64, dim)
-                        mask_spatial = final_mask_tgt.reshape(64, 64)
                         
-                        # Frequency Decomposition
                         tgt_low = get_fft_filter(tgt_spatial, self.freq_threshold, mode='lowpass')
                         tgt_high = get_fft_filter(tgt_spatial, self.freq_threshold, mode='highpass')
                         ref_high = get_fft_filter(ref_spatial, self.freq_threshold, mode='highpass')
                         
-                        # Style alignment
                         ref_high_styled = adain_style(ref_high, tgt_high)
-                        
-                        # Selective Injection: only within the mask
                         combined_spatial = tgt_low + (alpha * ref_high_styled + (1 - alpha) * tgt_high)
-                        
-                        # Update only masked pixels in the original output tensor
-                        # To avoid full spatial rewrite, we flatten and use mask
                         output[i][final_mask_tgt] = combined_spatial.reshape(-1, dim)[final_mask_tgt]
                     else:
-                        # Original Spatial Injection Path
                         other_outputs = old_output[curr_mapping][min_dists, curr_nn_map][final_mask_tgt]
-                        
-                        if self.background_adain is None:
-                            output[i][final_mask_tgt] = alpha * other_outputs + (1 - alpha)*old_output[i][final_mask_tgt]    
-                        elif self.background_adain == 'pre-subject':
-                            other_outputs = adain_style(other_outputs, old_output[i][final_mask_tgt])
-                            output[i][final_mask_tgt] = alpha * other_outputs + (1 - alpha)*old_output[i][final_mask_tgt]
-                        elif self.background_adain == 'pre-subject-background':
-                            other_outputs = adain_style(other_outputs, old_output[i][~final_mask_tgt])
-                            output[i][final_mask_tgt] = alpha * other_outputs + (1 - alpha)*old_output[i][final_mask_tgt]
-                        elif self.background_adain == 'post':
-                            style_reference = old_output[i][~final_mask_tgt]
-                            result = alpha * other_outputs + (1 - alpha)*old_output[i][final_mask_tgt]
-                            output[i][final_mask_tgt] = adain_style(result, style_reference)
+                        output[i][final_mask_tgt] = alpha * other_outputs + (1 - alpha)*old_output[i][final_mask_tgt]
 
             if anchors_cache and anchors_cache.is_cache_mode():
                 if place_in_unet not in anchors_cache.h_out_cache:
@@ -180,12 +151,9 @@ class FeatureInjector:
         bsz = output.shape[0]
         if not (curr_iter  >= self.background_self_alignment_range[0] and curr_iter <= self.background_self_alignment_range[1]):
             return output
-        
         if output_res != 64:
             return output
-
         attn_masks = self.attn_masks[output_res]
-
         for i in range(bsz):
             mask = attn_masks[i]
             style_reference = output[i][~mask]
@@ -194,8 +162,6 @@ class FeatureInjector:
 
     def inject_anchors(self, output, curr_iter, output_res, extended_mapping, place_in_unet, anchors_cache):
         curr_unet_part = place_in_unet.split('_')[0]
-
-        # Inject only in the specified unet parts (up, mid, down)
         if (curr_unet_part not in self.inject_unet_parts) or output_res not in self.inject_res:
             return output
 
@@ -208,43 +174,52 @@ class FeatureInjector:
         alpha = next((alpha for min_range, max_range, alpha in self.inject_range_alpha if min_range <= curr_iter <= max_range), None)
         if alpha:
             anchor_outputs = anchors_cache.h_out_cache[place_in_unet][curr_iter]
-            old_output = output#.clone()
+            old_output = output
             for i in range(bsz):
                 if self.swap_strategy == 'min':
                     min_dists = nn_distances[i].argmin(dim=0)
                     curr_nn_map = nn_map[i][min_dists, torch.arange(vector_dim)]
-
                     curr_nn_distances = nn_distances[i][min_dists, torch.arange(vector_dim)]
                     dist_thr = get_dynamic_threshold(curr_nn_distances) if self.dist_thr == 'dynamic' else self.dist_thr
                     dist_mask = curr_nn_distances < dist_thr
                     final_mask_tgt = attn_masks[i] & dist_mask
 
-                    other_outputs = anchor_outputs[min_dists, curr_nn_map][final_mask_tgt]
-                    output[i][final_mask_tgt] = alpha * other_outputs + (1 - alpha)*old_output[i][final_mask_tgt]
+                    if self.use_freq_decouple and output_res == 64:
+                        dim = output.shape[-1]
+                        tgt_spatial = output[i].reshape(64, 64, dim)
+                        ref_all = anchor_outputs[min_dists, curr_nn_map]
+                        ref_spatial = ref_all.reshape(64, 64, dim)
+                        
+                        tgt_low = get_fft_filter(tgt_spatial, self.freq_threshold, mode='lowpass')
+                        tgt_high = get_fft_filter(tgt_spatial, self.freq_threshold, mode='highpass')
+                        ref_high = get_fft_filter(ref_spatial, self.freq_threshold, mode='highpass')
+                        
+                        ref_high_styled = adain_style(ref_high, tgt_high)
+                        combined_spatial = tgt_low + (alpha * ref_high_styled + (1 - alpha) * tgt_high)
+                        output[i][final_mask_tgt] = combined_spatial.reshape(-1, dim)[final_mask_tgt]
+                    else:
+                        other_outputs = anchor_outputs[min_dists, curr_nn_map][final_mask_tgt]
+                        output[i][final_mask_tgt] = alpha * other_outputs + (1 - alpha)*old_output[i][final_mask_tgt]
 
         return output
 
 
 class AnchorCache:
     def __init__(self):
-        self.input_h_cache = {} # place_in_unet, iter, h_in
-        self.h_out_cache = {} # place_in_unet, iter, h_out
+        self.input_h_cache = {} 
+        self.h_out_cache = {} 
         self.anchors_last_mask = None
         self.dift_cache = None
-        self.mode = None # mode can be 'cache' or 'inject'
+        self.mode = None 
 
     def set_mode(self, mode):
         self.mode = mode
-
     def set_mode_inject(self):
         self.mode = 'inject'
-
     def set_mode_cache(self):
         self.mode = 'cache'
-
     def is_inject_mode(self):
         return self.mode == 'inject'
-
     def is_cache_mode(self):
         return self.mode == 'cache'
 
@@ -268,10 +243,8 @@ class QueryStore:
 
     def set_mode(self, mode): 
         self.mode = mode
-
     def cache_query(self, query, place_in_unet: str):
         self.query_store[place_in_unet] = query
-
     def inject_query(self, query, place_in_unet, t):
         if t >= self.t_range[0] and t <= self.t_range[1]:
             relative_t = t - self.t_range[0]
@@ -286,16 +259,13 @@ class DIFTLatentStore:
         self.steps = steps
         self.up_ft_indices = up_ft_indices
         self.dift_features = {}
-
     def __call__(self, features: torch.Tensor, t: int, layer_index: int):
         if t in self.steps and layer_index in self.up_ft_indices:
             self.dift_features[f'{int(t)}_{layer_index}'] = features
-
     def copy(self):
         copy_dift = DIFTLatentStore(self.steps, self.up_ft_indices)
         for key, value in self.dift_features.items():
             copy_dift.dift_features[key] = value.clone()
         return copy_dift
-
     def reset(self):
         self.dift_features = {}
